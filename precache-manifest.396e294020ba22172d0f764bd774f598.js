126bda4b42e0bdba93ef346c874e84e3e1826c5a
Stanley Chiu <hychiu2012@gmail.com>
2021-10-02 23:18:20 +0800

    Updates

---
+++ precache-manifest.396e294020ba22172d0f764bd774f598.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad4bf9d6bf560d458e14b241bb07e2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44016564f6463ad4dc33",
+    "url": "/static/js/2.9d27b3e7.chunk.js"
+  },
+  {
+    "revision": "b4b0bab7f8ae6af4ebda",
+    "url": "/static/js/main.4d0e058f.chunk.js"
+  },
+  {
+    "revision": "5043e13bdf52d7267c72",
+    "url": "/static/js/runtime-main.7181ce67.js"
+  }
+]);
